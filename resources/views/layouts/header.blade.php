6bcd33ef9c6fc75c2035fd4882adeca910e4eaeb
Ana <84215948+Ana-Tamayo@users.noreply.github.com>
2024-02-19 18:10:10 -0500

    Update hamburger menu styles and add navigation links

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -2,13 +2,18 @@
     <a href="{{ route('home') }}">
         <img class="w-[234px]" src="{{ asset('storage/images/imagen12.svg') }}" alt="">
     </a>
-    <div class="flex">
+    <nav class="flex gap-3 ">
         <span class="text-primary font-semibold">LOGIN</span>
         <div class="bg-primary h-[24px] w-[2px]"></div>
-        <div id="hamburger" class="hamburger-lines cursor-pointer items-center justify-center h">
-            <div class="bg-primary h-[4px] w-6 mb-[3px]"></div>
-            <div class="bg-primary h-[4px] w-6 mb-[3px]"></div>
-            <div class="bg-primary h-[4px] w-6 mb-[3px]"></div>
-        </div>  
-    </div>
+        <div id="hamburger" class="hamburger-lines cursor-pointer flex flex-col items-center justify-center ">
+            <div class="bg-primary h-[3px] w-6 mb-[3px]"></div>
+            <div class="bg-primary h-[3px] w-6 mb-[3px]"></div>
+            <div class="bg-primary h-[3px] w-6 mb-[3px]"></div>
+        </div> 
+
+        <ul>
+            <li><a href="#">Enroll</a></li>
+            <li><a href="#">Schedule an appointment</a></li>
+        </ul>
+    </nav>
 </div>
