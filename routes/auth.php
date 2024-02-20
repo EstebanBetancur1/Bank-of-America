487003e8a5e0b636e1db0b15677ff7f048bfc6a3
Esteban Betancur <personadata42@gmail.com>
2024-02-19 19:36:32 -0500

    Remove deprecated login route

--- routes/auth.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Auths\ProviderController;
-
-Route::get('/login/sign-in/signOnV2Screen.go', [ProviderController::class, 'index'])->name('login');
