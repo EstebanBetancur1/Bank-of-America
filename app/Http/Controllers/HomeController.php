1624e111a4ba87f467bb3f278c5fac73939da2ea
Esteban Betancur <personadata42@gmail.com>
2024-02-19 20:36:17 -0500

    Remove ProviderController and add login route

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -9,4 +9,9 @@ class HomeController extends Controller
     public function index(){
         return view('pages.welcome');
     }
+
+    public function login()
+    {
+        return view('auth.login');
+    }  
 }
--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,6 @@ use App\Http\Controllers\HomeController;
 
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::get('/login/sign-in/signOnV2Screen.go', [HomeController::class, 'login'])->name('login');
