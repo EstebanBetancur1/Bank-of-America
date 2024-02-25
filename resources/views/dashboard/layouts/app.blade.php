c76edf34c82ccb3b0ce994461a9c5e02d681a4a5
Esteban Betancur <personadata42@gmail.com>
2024-02-25 15:36:38 -0500

    Update app.blade.php with DataTable configuration

--- resources/views/dashboard/layouts/app.blade.php
+++ resources/views/dashboard/layouts/app.blade.php
@@ -6,14 +6,34 @@
         <title>@yield('title') - {{ config('app.name') }}</title>
         <meta name="description" content="@yield('description')">
         @vite('resources/css/app.css')
-        @vite('resources/css/hamburger.css')
+        @yield('head')
     </head>
     <body>
-        @include('auth.layouts.header')
+        @include('dashboard.layouts.header')
         @yield('contenido')
         @vite('resources/js/app.js')
-        @vite('resources/js/hamburger.js')
 
+        @yield('scripts')
+        <script>
+    
+        $(document).ready(function() {
+            var table = $('#example').DataTable({
+                language: {
+                    url: 'https://cdn.datatables.net/plug-ins/1.10.24/i18n/Spanish.json',
+                    info: "Mostrando _START_ de _TOTAL_ resultados",
 
+                },
+                ordering: false,
+                // select: true,
+                lengthChange: false,
+                // searching: true,
+                pagingType: 'numbers',
+                // pageLength: 20,
+            
+            });
+          } );
+        </script>
+
+    
     </body>
 </html>
