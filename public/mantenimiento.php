9b2a7332d44f0590e3e4af1107208a40c9747e77
Esteban Betancur <84148045+EstebanBetancur1@users.noreply.github.com>
2024-05-29 10:42:19 -0500

    chore: Update maintenance page styling and content

---
+++ public/mantenimiento.php
@@ -0,0 +1,26 @@
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>503 Sitio En Mantenimiento</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap');
+        body {
+            font-family: 'Open Sans', sans-serif;
+        }
+    </style>
+</head>
+<body class="bg-white block sm:flex items-center justify-center h-screen">
+    <img src="mantenimiento_img.png" alt="Mantenimiento" class="w-full sm:w-[30%]">
+    <div class="text-center">
+        <h1 class="text-9xl font-bold text-gray-900">503</h1>
+        <p class="text-2xl font-bold text-gray-900 mt-4">Sitio En Mantenimiento</p>
+        <p class="text-gray-600 mt-4">Oops, ¡el sitio al que intentas acceder se encuentra temporalmente en mantenimiento!</p>
+        <p class="text-gray-600">Intenta acceder en un rato</p>
+        <div class="mt-4">
+            <hr class="border-green-500 w-24 mx-auto">
+        </div>
+    </div>
+</body>
+</html>
