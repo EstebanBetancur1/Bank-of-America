b805b83e72cc93e211b799d0fbc6659a12895966
Esteban Betancur <84148045+EstebanBetancur1@users.noreply.github.com>
2024-09-19 10:03:56 -0500

    Refactor routes and views for user login functionality

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,7 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('pages.welcome');
+        return view('auth.login');
     }
 
     public function login()
